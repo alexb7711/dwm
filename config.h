96c6cf75edb1c7275867b3c754561191528764af
Alex Brown <alex.brown7711@gmail.com>
2020-03-31 10:17:54 -0600

    Made Inconsolata my font :)

--- config.h
+++ config.h
@@ -1,23 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 2;        /* border pixel of windows */
-static const unsigned int gappx    = 10;        /* gaps between windows */
-static const unsigned int snap     = 32;       /* snap pixel */
-static const int showbar           = 1;        /* 0 means no bar */
-static const int topbar            = 1;        /* 0 means bottom bar */
-static const char *fonts[]         = { "monospace:size                       = 10" };
-static const char dmenufont[]      = "monospace:size                         = 10";
-static const char normbg[]         = "#1d2021";
-static const char normborder[]     = "#1d2021";
-static const char normfg[]         = "#bbbbbb";
-static const char selfg[]          = "#eeeeee";
-static const char selbg[]          = "#458588";
-static const char selborder[]      = "#a89984";
-static const char *colors[][3]     = {
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Inconsolata Nerd Font Mono:size=11" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { normfg, normbg, normborder },
-	[SchemeSel]  = { selfg, selbg,  selborder  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
@@ -60,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbg, "-nf", normfg, "-sb", selbg, "-sf", selfg, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
@@ -85,9 +84,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	// { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	// { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-        // { MODKEY|ShiftMask,             XK_space,  setlayout,      {0} },
+	// { MODKEY,                       XK_space,  setlayout,      {0} },
         { MODKEY,                       XK_space,  spawn,          SHCMD("rofi -no-lazy-grab -show drun -theme launchers/style_normal_grid.rasi") },
-	// { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
