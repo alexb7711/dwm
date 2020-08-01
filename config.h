9efc0c7167612b76ad571e40074ad407ebcb0cd1
Alex Brown <alex.brown7711@gmail.com>
2020-08-01 13:12:08 -0600

    removed center window name

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Inconsolata Nerd Font Mono:size      = 11" };
 static const char dmenufont[]       = "monospace:size                         = 10";
