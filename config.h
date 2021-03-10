0c24365563f2b04bbfcf7d46f1458b5f48a68721
Alexander Brown <alex.brown@brownhaus.xyz>
2021-03-09 17:00:32 -0700

    Squashed commit of the following:
    commit 00f8ce2ff55b6e50b66eaf61eaf3c7dab98d8d85
    Author: Alexander Brown <alex.brown@brownhaus.xyz>
    Date:   Tue Mar 9 16:58:42 2021 -0700
    
        swallow
    
    commit fc96ade22f59caf270811b07fbf3a255ae4dc334
    Author: Alexander Brown <alex.brown@brownhaus.xyz>
    Date:   Sun Nov 22 12:33:36 2020 -0700
    
        Changed border color
    
    commit efcafff0df5b8da03e76fe4c376e5d62241ab3bf
    Author: Alexander Brown <alex.brown@brownhaus.xyz>
    Date:   Sun Nov 22 00:25:39 2020 -0700
    
        swallow

--- config.h
+++ config.h
@@ -32,14 +32,16 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class                        , instance , title          , tags mask , isfloating , isterminal , noswallow , monitor */
- 	{ "tabbed"                      , NULL     , "st"           , 0         , 0          , 1          , 0         , -1 }       ,
- 	{ "St"                          , NULL     , NULL           , 0         , 0          , 1          , 0         , -1 }       ,
-	{ "Gimp"                        , NULL     , NULL           , 0         , 1          , 0          , 0         , -1 }       ,
-	{ "Brave"                       , NULL     , NULL           , 0         , 0          , 0          , 1         , -1 }       ,
-	{ "MATLAB R2017b - student use" , NULL     , NULL           , 0         , 1          , 0          , 1         , -1 }       ,
-	{ "rviz"                        , NULL     , NULL           , 0         , 0          , 0          , 1         , -1 }       ,
- 	{ NULL                          , NULL     , "Event Tester" , 0         , 0          , 0          , 1         , -1 }       , /* xev */
+	/* class                         , instance , title          , tags mask , isfloating , isterminal , noswallow , monitor */
+ 	{ "tabbed"                       , NULL     , "st"           , 0         , 0          , 1          , 0         , -1 }       ,
+ 	{ "St"                           , NULL     , NULL           , 0         , 0          , 1          , 0         , -1 }       ,
+	{ "Gimp"                         , NULL     , NULL           , 0         , 1          , 0          , 0         , -1 }       ,
+	{ "Brave"                        , NULL     , NULL           , 0         , 0          , 0          , 1         , -1 }       ,
+	{ "MATLAB R2020b - academic use" , NULL     , NULL           , 0         , 1          , 0          , 1         , -1 }       ,
+	{ "rviz"                         , NULL     , NULL           , 0         , 0          , 0          , 1         , -1 }       ,
+	{ "slack"                        , NULL     , NULL           , 0         , 0          , 0          , 1         , -1 }       ,
+	{ "p3x-onenote"                  , NULL     , NULL           , 0         , 0          , 0          , 1         , -1 }       ,
+ 	{ NULL                           , NULL     , "Event Tester" , 0         , 0          , 0          , 1         , -1 }       , /* xev */
 };
 
 /* layout(s) */
