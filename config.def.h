68334f2f86a9934d8b6aaaa428e345728bbaba59
Alexander Brown <alex.brown@brownhaus.xyz>
2022-03-07 10:04:16 -0700

    update 03-07-2022

--- config.def.h
+++ config.def.h
@@ -3,11 +3,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx    = 2;        /* border pixel of windows */
+static const unsigned int borderpx    = 3;        /* border pixel of windows */
 static const unsigned int gappx       = 5;        /* gaps between windows */
 static const unsigned int snap        = 32;       /* snap pixel */
 static const int swallowfloating      = 0;        /* 1 means swallow floating windows by default */
-static const int showbar              = 0;        /* 0 means no bar */
+static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 1;        /* 0 means bottom bar */
 static const char *fonts[]            = { "Inconsolata Nerd Font Mono:size                          = 11" };
 static const char dmenufont[]         = "Inconsolata Nerd Font Mono:size                            = 11"  ;
@@ -16,8 +16,7 @@ static const char normborder[]        = "#282828";
 static const char normfg[]            = "#eeeeee";
 static const char selfg[]             = "#eeeeee";
 static const char selbg[]             = "#458588";
-//static const char selborder[]       = "#a89984";
-static const char selborder[]         = "#d79921";
+static const char selborder[]         = "#689d6a";
 static const char *colors[][3]        = {
 	/*               fg     , bg     , border       , */
 	[SchemeNorm] = { normfg , normbg , normborder } ,
@@ -45,12 +44,14 @@ static const Rule rules[] = {
 	{ "Gimp"                        , NULL     , NULL           , 0         , 1          , 0          , 0         , -1      , 0   } ,
 	{ "MATLAB R2020b - academic use", NULL     , NULL           , 0         , 1          , 0          , 1         , -1      , 0   } ,
 	{ "rviz"                        , NULL     , NULL           , 0         , 0          , 0          , 1         , -1      , 0   } ,
-	{ NULL                          , NULL     , "scratchpad"   , 0         , 1          , 0          , 1         , -1      , 's' } ,
 	{ NULL                          , NULL     , "ncspot"       , 0         , 1          , 0          , 1         , -1      , 'n' } ,
 	{ NULL                          , NULL     , "pulsemixer"   , 0         , 1          , 0          , 1         , -1      , 'p' } ,
- 	{ "St"                          , NULL     , NULL           , 0         , 0          , 1          , 0         , -1      , 0   } ,
- 	{ "tabbed"                      , NULL     , "st"           , 0         , 0          , 1          , 0         , -1      , 0   } ,
+	{ NULL                          , NULL     , "scratchpad"   , 0         , 1          , 0          , 1         , -1      , 's' } ,
+ 	{ "St"                          , NULL     , NULL           , 0         , 0          , 1          , 1         , -1      , 0   } ,
  	{ "obs"                         , NULL     , NULL           , 1 << 8    , 0          , 1          , 0         , -1      , 0   } ,
+ 	{ "tabbed"                      , NULL     , "st"           , 0         , 0          , 1          , 0         , -1      , 0   } ,
+ 	{ NULL                          , NULL     , "MAV Viewer"   , 0         , 0          , 0          , 1         , -1      , 0   } ,
+ 	{ NULL                          , NULL     , "States"       , 0         , 0          , 0          , 1         , -1      , 0   } ,
  	{ NULL                          , NULL     , "Event Tester" , 0         , 0          , 0          , 1         , -1      , 0   } , /* xev */
 };
 
@@ -80,11 +81,11 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2]        = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbg, "-nf", normfg, "-sb", selbg, "-sf", selfg, NULL };
-static const char *clpmnucmd[] = { "clipmenu", NULL };
-static const char *termcmd[]   = { "tabbed", "-r", "2", "-c", "st", "-w", "''", NULL};
-static const char *surfcmd[]   = { "tabbed", "-c", "surf", "-e", NULL};
+static char dmenumon[2]         = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbg, "-nf", normfg, "-sb", selbg, "-sf", selfg, NULL };
+static const char *clpmnucmd[]  = { "clipmenu", NULL };
+static const char *termcmd[]    = { "tabbed", "-r", "2", "-c", "st", "-w", "''", NULL};
+static const char *browsercmd[] = { "brave", NULL};
 
 /*First arg only serves to match against key in rules*/
 static const char *scratchpadcmd[] = {"s" , "st" , "-t" , "scratchpad" , NULL};
@@ -103,7 +104,6 @@ static const char *next[]       = {"playerctl", "next"};
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                          , key                      , function       , argument */
-       { MODKEY|ShiftMask                    , XK_l                     , spawn          , SHCMD("dm-logout") }                        ,
        { 0                                   , XF86XK_AudioRaiseVolume  , spawn          , SHCMD("audioSwapper up") }                  ,
        { 0                                   , XF86XK_AudioLowerVolume  , spawn          , SHCMD("audioSwapper down") }                ,
        { 0                                   , XF86XK_AudioMute         , spawn          , SHCMD("audioSwapper mute") }                ,
@@ -115,17 +115,18 @@ static Key keys[] = {
        { 0                                   , XF86XK_AudioPrev         , spawn          , {.v = previous} }                           ,
        { MODKEY                              , XK_Return                , spawn          , {.v = termcmd } }                           ,
        { MODKEY                              , XK_b                     , togglebar      , {0} }                                       ,
-       // { MODKEY|ShiftMask                 , XK_b                     , spawn          , SHCMD("brave") }                            ,
-       { MODKEY|ShiftMask                    , XK_b                     , spawn          , {.v = surfcmd} }                            ,
+       { MODKEY|ShiftMask                    , XK_b                     , spawn          , SHCMD("dm-bluetooth")}                      ,
        { MODKEY                              , XK_c                     , setlayout      , {.v = &layouts[2]} }                        ,
        { MODKEY|ShiftMask                    , XK_c                     , spawn          , {.v = clpmnucmd}}                           ,
-       { MODKEY                              , XK_e                     , spawn          , SHCMD("tabbed -r 2 st -w '' -e \"nvim\"") } ,
+       { MODKEY|ShiftMask                    , XK_d                     , spawn          , SHCMD("dm-kill") }                          ,
        { MODKEY                              , XK_j                     , focusstack     , {.i = +1 } }                                ,
        { MODKEY                              , XK_k                     , focusstack     , {.i = -1 } }                                ,
        { MODKEY                              , XK_i                     , incnmaster     , {.i = +1 } }                                ,
+       { MODKEY|ShiftMask                    , XK_i                     , spawn          , {.v = browsercmd} }                         ,
        { MODKEY                              , XK_d                     , incnmaster     , {.i = -1 } }                                ,
        { MODKEY                              , XK_h                     , setmfact       , {.f = -0.05} }                              ,
        { MODKEY                              , XK_l                     , setmfact       , {.f = +0.05} }                              ,
+       { MODKEY|ShiftMask                    , XK_l                     , spawn          , SHCMD("dm-logout") }                        ,
        { MODKEY|ShiftMask                    , XK_j                     , movestack      , {.i = +1 } }                                ,
        { MODKEY|ShiftMask                    , XK_k                     , movestack      , {.i = -1 } }                                ,
        { MODKEY                              , XK_f                     , togglefullscr  , {0} }                                       ,
@@ -133,9 +134,8 @@ static Key keys[] = {
        { MODKEY                              , XK_t                     , setlayout      , {.v = &layouts[0]} }                        ,
        { MODKEY                              , XK_m                     , spawn          , SHCMD("dm-man")}                            ,
        { MODKEY|ShiftMask                    , XK_m                     , spawn          , SHCMD("monitorSwapper")}                    ,
-	// { MODKEY                          , XK_o                     , setlayout      , {.v = &layouts[4]} }                        ,
+       { MODKEY                              , XK_o                     , spawn          , SHCMD("~/Code/scripts/dmenu/dm-books") }    ,
 	// { MODKEY                          , XK_m                     , setlayout      , {.v = &layouts[2]} }                        ,
-       { MODKEY                              , XK_s                     , spawn          , SHCMD("dm-kill") }                          ,
        { MODKEY|ShiftMask                    , XK_s                     , spawn          , SHCMD("screenshot")}                        ,
        { MODKEY|ShiftMask                    , XK_w                     , spawn          , SHCMD("setWallpaper")}                      ,
        { MODKEY                              , XK_y                     , spawn          , SHCMD("dm-youtube")}                        ,
@@ -144,13 +144,13 @@ static Key keys[] = {
        { MODKEY|ShiftMask                    , XK_space                 , togglefloating , {0} }                                       ,
        { MODKEY                              , XK_0                     , view           , {.ui = ~0 } }                               ,
        { MODKEY|ShiftMask                    , XK_0                     , tag            , {.ui = ~0 } }                               ,
-       { MODKEY                              , XK_comma                 , focusmon       , {.i = +1 } }                                ,
-       { MODKEY                              , XK_period                , focusmon       , {.i = -1 } }                                ,
+       { MODKEY                              , XK_comma                 , focusmon       , {.i = -1 } }                                ,
+       { MODKEY                              , XK_period                , focusmon       , {.i = +1 } }                                ,
        { MODKEY                              , XK_minus                 , setgaps        , {.i = -1 } }                                ,
        { MODKEY                              , XK_equal                 , setgaps        , {.i = +1 } }                                ,
        { MODKEY|ShiftMask                    , XK_equal                 , setgaps        , {.i = 0  } }                                ,
-       { MODKEY|ShiftMask                    , XK_comma                 , tagmon         , {.i = +1 } }                                ,
-       { MODKEY|ShiftMask                    , XK_period                , tagmon         , {.i = -1 } }                                ,
+       { MODKEY|ShiftMask                    , XK_comma                 , tagmon         , {.i = -1 } }                                ,
+       { MODKEY|ShiftMask                    , XK_period                , tagmon         , {.i = +1 } }                                ,
        { MODKEY                              , XK_F1                    , togglescratch  , {.v = scratchpadcmd } }                     ,
        { MODKEY                              , XK_F2                    , togglescratch  , {.v = ncspotify     } }                     ,
        { MODKEY                              , XK_F3                    , togglescratch  , {.v = pulsemixer    } }                     ,
@@ -183,4 +183,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
