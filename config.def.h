1d99827db5745734400d52bbfe6708d86c98dbc0
Alexander Brown <alex.brown7711@gmail.com>
2023-04-14 08:27:51 -0600

    Update config

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const unsigned int gappx       = 5;        /* gaps between windows */
 static const unsigned int snap        = 32;       /* snap pixel */
 static const int swallowfloating      = 0;        /* 1 means swallow floating windows by default */
 static const int showbar              = 1;        /* 0 means no bar */
-static const int topbar               = 1;        /* 0 means bottom bar */
+static const int topbar               = 0;        /* 0 means bottom bar */
 static const char *fonts[]            = { "Inconsolata Nerd Font Mono:size                          = 11" };
 static const char dmenufont[]         = "Inconsolata Nerd Font Mono:size                            = 11"  ;
 static const char normbg[]            = "#282828";
@@ -18,41 +18,39 @@ static const char selfg[]             = "#eeeeee";
 static const char selbg[]             = "#458588";
 static const char selborder[]         = "#689d6a";
 static const char *colors[][3]        = {
-	/*               fg     , bg     , border       , */
-	[SchemeNorm] = { normfg , normbg , normborder } ,
-	[SchemeSel]  = { selfg  , selbg  , selborder  } ,
+  /*               fg     , bg     , border       , */
+  [SchemeNorm] = { normfg , normbg , normborder } ,
+  [SchemeSel]  = { selfg  , selbg  , selborder  } ,
 };
 
 static const unsigned int baralpha    = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const unsigned int alphas[][3] = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+  /*               fg      bg        border     */
+  [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+  [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class                        , instance , title          , tags mask , isfloating , isterminal , noswallow , monitor , scratch key */
-	{ "Brave"                       , NULL     , NULL           , 0         , 0          , 0          , 1         , -1      , 0   } ,
-	{ "Gimp"                        , NULL     , NULL           , 0         , 1          , 0          , 0         , -1      , 0   } ,
-	{ "MATLAB R2020b - academic use", NULL     , NULL           , 0         , 1          , 0          , 1         , -1      , 0   } ,
-	{ "rviz"                        , NULL     , NULL           , 0         , 0          , 0          , 1         , -1      , 0   } ,
-	{ NULL                          , NULL     , "ncspot"       , 0         , 1          , 0          , 1         , -1      , 'n' } ,
-	{ NULL                          , NULL     , "pulsemixer"   , 0         , 1          , 0          , 1         , -1      , 'p' } ,
-	{ NULL                          , NULL     , "scratchpad"   , 0         , 1          , 0          , 1         , -1      , 's' } ,
- 	{ "St"                          , NULL     , NULL           , 0         , 0          , 1          , 1         , -1      , 0   } ,
- 	{ "obs"                         , NULL     , NULL           , 1 << 8    , 0          , 1          , 0         , -1      , 0   } ,
- 	{ "tabbed"                      , NULL     , "st"           , 0         , 0          , 1          , 0         , -1      , 0   } ,
- 	{ NULL                          , NULL     , "MAV Viewer"   , 0         , 0          , 0          , 1         , -1      , 0   } ,
- 	{ NULL                          , NULL     , "States"       , 0         , 0          , 0          , 1         , -1      , 0   } ,
- 	{ NULL                          , NULL     , "Event Tester" , 0         , 0          , 0          , 1         , -1      , 0   } , /* xev */
+  /* xprop(1):
+   *  WM_CLASS(STRING) = instance, class
+   *  WM_NAME(STRING) = title
+   */
+  /* class                        , instance , title          , tags mask , isfloating , isterminal , noswallow , monitor , scratch key */
+  { "Brave"                       , NULL     , NULL           , 0         , 0          , 0          , 1         , -1      , 0   } ,
+  { "Gimp"                        , NULL     , NULL           , 0         , 1          , 0          , 0         , -1      , 0   } ,
+  { NULL                          , NULL     , "ncspot"       , 0         , 1          , 0          , 1         , -1      , 'n' } ,
+  { NULL                          , NULL     , "pulsemixer"   , 0         , 1          , 0          , 1         , -1      , 'p' } ,
+  { NULL                          , NULL     , "scratchpad"   , 0         , 1          , 0          , 1         , -1      , 's' } ,
+  { "St"                          , NULL     , NULL           , 0         , 0          , 1          , 1         , -1      , 0   } ,
+  { "obs"                         , NULL     , NULL           , 1 << 8    , 0          , 1          , 0         , -1      , 0   } ,
+  { "tabbed"                      , NULL     , "st"           , 0         , 0          , 1          , 0         , -1      , 0   } ,
+  { NULL                          , NULL     , "MAV Viewer"   , 0         , 0          , 0          , 1         , -1      , 0   } ,
+  { NULL                          , NULL     , "States"       , 0         , 0          , 0          , 1         , -1      , 0   } ,
+  { NULL                          , NULL     , "Event Tester" , 0         , 0          , 0          , 1         , -1      , 0   } , /* xev */
 };
 
 /* layout(s) */
@@ -62,21 +60,21 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "[=]",      tile },    /* first entry is default */
+  /* symbol     arrange function */
+  { "[=]",      tile },    /* first entry is default */
         { "[~]",      NULL },    /* [> no layout function means floating behavior <] */
-	// { "[M]",      monocle },
-	{ "[M]",      centeredmaster },
-	// { ">M>",      centeredfloatingmaster },
+  // { "[M]",      monocle },
+  { "[M]",      centeredmaster },
+  // { ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+  { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+  { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+  { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+  { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -105,7 +103,7 @@ static const char *next[]       = {"playerctl", "next"};
 
 #include "movestack.c"
 static Key keys[] = {
-	/* modifier                          , key                      , function       , argument */
+  /* modifier                          , key                      , function       , argument */
        { 0                                   , XF86XK_AudioRaiseVolume  , spawn          , SHCMD("audioSwapper up") }                  ,
        { 0                                   , XF86XK_AudioLowerVolume  , spawn          , SHCMD("audioSwapper down") }                ,
        { 0                                   , XF86XK_AudioMute         , spawn          , SHCMD("audioSwapper mute") }                ,
@@ -137,9 +135,10 @@ static Key keys[] = {
        { MODKEY                              , XK_t                     , setlayout      , {.v = &layouts[0]} }                        ,
        { MODKEY                              , XK_m                     , spawn          , SHCMD("dm-man")}                            ,
        { MODKEY|ShiftMask                    , XK_m                     , spawn          , SHCMD("monitorSwapper")}                    ,
-       { MODKEY                              , XK_o                     , spawn          , SHCMD("~/Code/scripts/dmenu/dm-books") }    ,
-	// { MODKEY                          , XK_m                     , setlayout      , {.v = &layouts[2]} }                        ,
+       { MODKEY                              , XK_o                     , spawn          , SHCMD("~/code/scripts/dmenu/dm-books") }    ,
+  // { MODKEY                          , XK_m                     , setlayout      , {.v = &layouts[2]} }                        ,
        { MODKEY|ShiftMask                    , XK_s                     , spawn          , SHCMD("screenshot")}                        ,
+       { MODKEY|ShiftMask                    , XK_v                     , spawn          , SHCMD("~/code/scripts/ezw")}                ,
        { MODKEY|ShiftMask                    , XK_w                     , spawn          , SHCMD("setWallpaper")}                      ,
        { MODKEY                              , XK_y                     , spawn          , SHCMD("dm-youtube")}                        ,
        { MODKEY                              , XK_Tab                   , view           , {0} }                                       ,
@@ -157,15 +156,15 @@ static Key keys[] = {
        { MODKEY                              , XK_F1                    , togglescratch  , {.v = scratchpadcmd } }                     ,
        { MODKEY                              , XK_F2                    , togglescratch  , {.v = ncspotify     } }                     ,
        { MODKEY                              , XK_F3                    , togglescratch  , {.v = pulsemixer    } }                     ,
-	TAGKEYS(                        XK_1 , 0)
-	TAGKEYS(                        XK_2 , 1)
-	TAGKEYS(                        XK_3 , 2)
-	TAGKEYS(                        XK_4 , 3)
-	TAGKEYS(                        XK_5 , 4)
-	TAGKEYS(                        XK_6 , 5)
-	TAGKEYS(                        XK_7 , 6)
-	TAGKEYS(                        XK_8 , 7)
-	TAGKEYS(                        XK_9 , 8)
+  TAGKEYS(                        XK_1 , 0)
+  TAGKEYS(                        XK_2 , 1)
+  TAGKEYS(                        XK_3 , 2)
+  TAGKEYS(                        XK_4 , 3)
+  TAGKEYS(                        XK_5 , 4)
+  TAGKEYS(                        XK_6 , 5)
+  TAGKEYS(                        XK_7 , 6)
+  TAGKEYS(                        XK_8 , 7)
+  TAGKEYS(                        XK_9 , 8)
        { MODKEY                              , XK_q                     , killclient     , {0} }                                       ,
        { MODKEY|ShiftMask                    , XK_q                     , quit           , {0} }                                       ,
 };
@@ -173,16 +172,16 @@ static Key keys[] = {
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+  /* click                event mask      button          function        argument */
+  { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+  { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+  { ClkWinTitle,          0,              Button2,        zoom,           {0} },
+  { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+  { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+  { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+  { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+  { ClkTagBar,            0,              Button1,        view,           {0} },
+  { ClkTagBar,            0,              Button3,        toggleview,     {0} },
+  { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+  { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
