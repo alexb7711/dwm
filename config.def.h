8a988b7eb299f12700a0cc31a286ef7a998e1eb7
Alexander Brown <alex.brown7711@gmail.com>
2025-06-19 12:46:46 -0600

    remove debug and use spacemono

--- config.def.h
+++ config.def.h
@@ -9,10 +9,8 @@ static const unsigned int snap = 32;    /* snap pixel */
 static const int swallowfloating = 0;   /* 1 means swallow floating windows by default */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 0;  /* 0 means bottom bar */
-// static const char *fonts[] = {"Inconsolata Nerd Font Mono :size = 11"};
-// static const char dmenufont[] = "Inconsolata Nerd Font Mono :size = 11";
-static const char *fonts[] = {"Inconsolata Nerd Font Mono:size=11"};
-static const char dmenufont[] = "Inconsolata Nerd Font Mono:size=11";
+static const char *fonts[] = {"SpaceMono Nerd Font Mono:size=11"};
+static const char dmenufont[] = "SpaceMono Nerd Font Mono:size=11";
 static const char normbg[] = "#282828";
 static const char normborder[] = "#282828";
 static const char normfg[] = "#eeeeee";
